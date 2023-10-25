23d4ad065c0978edfeb29087b4c3c6e08bbea495
roseeen <rneilrosen@gmail.com>
2023-10-25 18:15:01 +0800

    lessons

--- lesson03.php
+++ lesson03.php
@@ -1,53 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lessom3</title>
-</head>
-<body>
-    
 <?php
-    #trying
-     print<<<END
-     wala lang
-     END;
 
-     /* 
-     try lang
-    */
+/* Multi line comment
+testing 
+for lesson 3 SYSNTAXs
+*/
+echo  "Welcome to Php<br>";
+ $capital = 58;
+ $Capital = 50;
+ print("Variable capital is $capital<br>");
+ print("Variable CaPiTal is $Capital<br>");
  
-    print "Helloooooo";
+ $greetings = "Welcome to Php!";
+ 
+ if (3 == 2 + 1)
+ print("Good - I haven't totally lost my mind.<br>");
 
-?>
+ if (3 == 2 + 1)
+{
+ print("Good - I haven't totally");
+ print("lost my mind.<br>");
+}
 
-<?php
-    $four = 2 + 2;//single spaces
-    $four   =   2   +   2 ;//spaces and tabs
-    $four =
-    2+
-    2;// multiple lines
-    ?>
-
-    <?php 
-    $capital = 67;
-    print("Variable capital is $capital<br>");
-    print("Variable capital is $CaPiTaL<br>");
-
-    $greeting = "Welcome to PHP!";
-    ?>
-
-   <?php
-   if(3 == 2 + 1)
-    print("frfr<br>");
-
-    if(3 == 2 + 1)
-    {
-        print("frfr");
-        print("ngl<br>");
-    }
-   ?> 
-
-
-</body>
-</html>
+ ?>
--- lesson05.php
+++ lesson05.php
@@ -1,48 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lesson5</title>
-</head>
-<body>
-    <?php 
-    define("MINSIZE", 50);
-    echo MINSIZE;
-    echo constant("MINSIZE"); // same thing as the previous line
-    ?>
+<?php
+define("PI", 3.14);
+echo PI;
 
-    <?php   
-    // Valid constant names
-    define("ONE", "first thing");
-    define("TWO2", "second thing");
-    define("THREE_3", "third thing")
-    // Invalid constant names
-    define("2TWO", "second thing");
-    define("__THREE__", "third value");
-
-
-    ?>
-
-    <?php 
-    $a = 11;
-    $b = 12;
-   
-    $c = $a + $b;
-    echo "Addition Operation Result: $c <br/>";
-    $c = $a - $b;
-    echo "Subtraction Operation Result: $c <br/>";
-    $c = $a * $b;
-    echo "Multiplication Operation Result: $c <br/>";
-    $c = $a / $b;
-    echo "Division Operation Result: $c <br/>";
-    $c = $a % $b;
-    echo "Modulus Operation Result: $c <br/>";
-    $c = $a++;
-    echo "Increment Operation Result: $c <br/>";
-    $c = $a--;
-    echo "Decrement Operation Result: $c <br/>";
-    ?>
-
-</body>
-</html>
+?>
